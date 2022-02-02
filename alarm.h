a59fd4f1c0233846c873adc8f4c5f7498920168b
Henrik <henrik.lovhaug@gmail.com>
2022-02-02 21:32:59 +0100

    Minor css changes

--- alarm.h
+++ alarm.h
@@ -6,7 +6,6 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-
 #define MAX_ALARMS 100
 #define DATE_FORMAT "%Y-%m-%d %H:%M:%S"
 
@@ -15,7 +14,7 @@
 struct alarm
 {
     time_t time;
-    int pid;
+    pid_t pid;
 };
 
 struct alarm alarms[MAX_ALARMS];
@@ -41,6 +40,7 @@ void print_active_alarms()
     {
         if (alarms[i].pid != 0)
         {
+            printf("Pid is %d\n", alarms[i].pid);
             char time_str[32];
             struct tm tm_time;
             tm_time = *localtime(&alarms[i].time);
@@ -54,15 +54,32 @@ void set_alarm_x_seconds_from_now(int seconds)
 {
     struct alarm a;
     a.time = time(NULL) + seconds;
-    //Create child process
-    int local_pid = fork();
+    // Create child process
+    pid_t p[2];
+    if(pipe(p) < 0) exit(1);
+
+    pid_t local_pid = fork();
     if (local_pid == 0)
     {
-        while(time(NULL) < a.time)
+        pid_t pid = getpid();
+        write(p[1], &pid, sizeof(a));
+        close(p[1]);
+        close(p[0]);
+        while (time(NULL) < a.time)
         {
             sleep(1);
         }
         printf("Alarm!\n");
         exit(0);
+    }
+    else
+    {
+        close(p[1]);
+        pid_t c;
+        read(p[0], &c, sizeof(c));
+        a.pid = c;
+        alarms[idx] = a;
+        idx++;
+        close(p[0]);
     }
 }
