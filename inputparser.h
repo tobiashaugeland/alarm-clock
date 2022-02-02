9aaa75e31b1c52ae7bf9f6235e6a6e9057b19536
tobiashaugeland <tobias.haugeland@gmail.com>
2022-02-02 14:09:15 +0100

    cleanup

--- inputparser.h
+++ inputparser.h
@@ -9,21 +9,13 @@ int parseinput()
     if(strcmp(&inp, "s") == 0)
     {
         printf("Please enter the time you want to set the alarm to\n");
-        char time[9];
+        char time[32];
         scanf("%s", &time);
-        printf("Please enter the message you want to be displayed\n");
-        char message[100];
-        scanf("%s", &message);
-        printf("You have set the alarm to %s with the message %s\n", time, message);
-        return 1;
     }
     else if(strcmp(&inp, "l") == 0)
     {
         printf("You have the following alarms set:\n");
-        printf("Alarm 1: 2022-01-31 14:15:23\n");
-        printf("Alarm 2: 2022-01-31 14:15:23\n");
-        printf("Alarm 3: 2022-01-31 14:15:23\n");
-        return 1;
+
     }
     else if(strcmp(&inp, "c") == 0)
     {
@@ -31,7 +23,6 @@ int parseinput()
         int alarm;
         scanf("%d", &alarm);
         printf("You have cancelled alarm %d\n", alarm);
-        return 1;
     }
     else if(strcmp(&inp, "x") == 0)
     {
@@ -41,7 +32,6 @@ int parseinput()
     else
     {
         printf("Please enter a valid input\n");
-        return 1;
     }
     return 1;
 }
